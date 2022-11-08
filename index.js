3f3f2fd60815712ac960952a4750702460b20692
ghazalrb98 <ghazalrb.98@gmail.com>
2022-11-08 13:19:58 +0100

    Add remove order option

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const orderPrice = document.getElementById("order-price");
 document.addEventListener("click", function (e) {
   if (e.target.dataset.add) {
     handleAddClick(e.target.dataset.add);
+  } else if (e.target.dataset.remove) {
+    handleRemoveOrderItem(e.target.dataset.remove);
   }
 });
 
@@ -17,16 +19,24 @@ function handleAddClick(menuOptionId) {
     return menuOption.id === menuOptionId;
   })[0];
   orderItemsContainer.innerHTML += `
-    <div class="order-item-row">
+    <div id="order-${targetMenuOption.id}" class="order-item-row">
       <div class="order-item">
         <h3 class="order-item-title">${targetMenuOption.name}</h3>
-        <div class="order-item-remove-btn">remove</div>
+        <div class="order-item-remove-btn" data-remove="${targetMenuOption.id}">remove</div>
       </div>
       <p class="price">$${targetMenuOption.price}</p>
     </div>
   `;
 
   calculateTotalPrice(targetMenuOption.price);
+}
+
+function handleRemoveOrderItem(orderItemRowId) {
+  const orderItemRow = document.getElementById(`order-${orderItemRowId}`);
+  orderItemRow.remove();
+  if (orderItemsContainer.children.length === 0) {
+    orderContainer.classList.add("hidden");
+  }
 }
 
 function calculateTotalPrice(newPrice) {
